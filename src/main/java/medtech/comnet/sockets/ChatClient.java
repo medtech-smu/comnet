64c73d7080257c3876f156c9d6ba64566155fa9d
firas <firas>
2017-02-12 14:58:32 +0100

    initial commit

---
+++ src/main/java/medtech/comnet/sockets/ChatClient.java
@@ -0,0 +1,25 @@
+package medtech.comnet.sockets;
+
+import java.io.*;
+import java.net.*;
+
+public class ChatClient {
+
+	public static void main(String[] args) {
+		try {
+			Socket skt = new Socket("localhost", 1234);
+			BufferedReader in = new BufferedReader(new
+					InputStreamReader(skt.getInputStream()));
+			System.out.print("Received string: '");
+			while (!in.ready()) {}
+			System.out.println(in.readLine());
+			System.out.print("'\n");
+			in.close();
+			skt.close();
+		}
+		catch(Exception e) {
+			System.out.print("Whoops! It didn't work!\n");
+		}
+	}
+
+}
---
+++ src/main/java/medtech/comnet/sockets/EchoServer.java
@@ -0,0 +1,42 @@
+package medtech.comnet.sockets;
+
+import java.net.*;
+import java.io.*;
+
+public class EchoServer {
+
+	public static void main(String[] args) {
+		ServerSocket sock = null;
+		BufferedReader fromClient = null;
+		OutputStreamWriter toClient = null;
+		Socket client = null;
+		try
+		{
+			sock = new ServerSocket(4000); System.out.println("Server Ready");
+			client = sock.accept(); System.out.println("Client Connected");
+			fromClient = new BufferedReader(new
+					InputStreamReader(client.getInputStream()));
+			toClient = new OutputStreamWriter(client.getOutputStream());
+			String line;
+			while (true)
+			{
+				line = fromClient.readLine();
+				if ( (line == null) || line.equals("bye"))
+					break;
+				System.out.println ("Client [ " + line + " ]");
+				toClient.write("Server [ "+ line +" ]\n");
+				toClient.flush();
+			}
+			fromClient.close();
+			toClient.close();
+			client.close();
+			sock.close();
+			System.out.println("Client Disconnected");
+		}
+		catch (IOException ioe)
+		{
+			System.err.println(ioe);
+		}
+	}
+
+}
