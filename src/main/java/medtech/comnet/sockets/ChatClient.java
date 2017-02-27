9418267a462477798a71a1bde529c7406c9ea3f5
soumaya <rebai.soumaya@gmail.com>
2017-02-27 14:52:26 +0100

    Refactor code

--- src/main/java/medtech/comnet/sockets/ChatClient.java
+++ src/main/java/medtech/comnet/sockets/ChatClient.java
@@ -4,45 +4,92 @@ import java.io.*;
 import java.net.*;
 
 public class ChatClient {
+	private Socket socket;
+	private BufferedReader fromServer;
+	private BufferedReader fromUser;
+	private PrintWriter toServer;
+	private InetAddress serverIP;
+	private final int port = 4000;
 
-	public static void main(String[] args) {
-		BufferedReader fromServer = null, fromUser = null;
-		PrintWriter toServer = null;
-		Socket sock = null;
-		try
-		{
-			if (args.length == 0)
-				sock = new Socket(InetAddress.getLocalHost(),4000);
-			else
-				sock = new Socket(InetAddress.getByName(args[0]),4000);
-			fromServer = new BufferedReader(new
-					InputStreamReader(sock.getInputStream()));
+	public ChatClient() {
+	}
+
+	public void init() {
+		socket = null;
+		fromServer = null;
+		fromUser = null;
+		toServer = null;
+		serverIP = null;
+	}
+
+	public void setup() {
+		try {
+			if (serverIP == null)
+				socket = new Socket(InetAddress.getLocalHost(), port);
+
+			fromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			fromUser = new BufferedReader(new InputStreamReader(System.in));
-			toServer = new PrintWriter(sock.getOutputStream(),true);
-			String Usrmsg, Srvmsg;
-			System.out.println("Type \"bye\" to quit");
-			while (true)
-			{
-				System.out.print("Enter msg to server : ");
-				Usrmsg = fromUser.readLine();
-				if (Usrmsg==null || Usrmsg.equals("bye"))
-				{
-					toServer.println("bye"); break;
-				}
-				else
-					toServer.println(Usrmsg);
-				Srvmsg = fromServer.readLine();
-				System.out.println(Srvmsg);
-			}
-			fromUser.close();
-			fromServer.close();
-			toServer.close();
-			sock.close();
-		}
-		catch (IOException ioe)
-		{
-			System.err.println(ioe);
+			toServer = new PrintWriter(socket.getOutputStream(), true);
+
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
+	public void exec() {
+		String Usrmsg = "";
+		String Srvmsg = "";
+		System.out.println("Type \"bye\" to quit");
+		while (true) {
+			System.out.print("Enter msg to server : ");
+			try {
+				Usrmsg = fromUser.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			if (Usrmsg == null || Usrmsg.equals("bye")) {
+				toServer.println("bye");
+				break;
+			} else
+				toServer.println(Usrmsg);
+			try {
+				Srvmsg = fromServer.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println(Srvmsg);
+		}
+	}
+
+	public void close() {
+		try {
+			fromUser.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		try {
+			fromServer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		toServer.close();
+		try {
+			socket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		ChatClient client = new ChatClient();
+		try {
+			client.setup();
+			client.exec();
+		} finally {
+			client.close();
+		}
+	}
 }
