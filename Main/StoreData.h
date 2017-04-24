8b4190f795bc418a930c9b4c7a1ef9d7d76d93a4
FediHachicha <fedi.hachicha95@gmail.com>
2017-04-24 15:21:41 +0200

    Update StoreData.h

--- Main/StoreData.h
+++ Main/StoreData.h
@@ -4,12 +4,12 @@ char server[] = "**.**.**.**";
 WiFiClient client;
 
 void saveData (String msg){
-    if (client.connect(server, 80)) {
-        client.print("GET /write_data.php?"); // This
-        client.print("detail="); // This
-        client.print(msg); // And this is what we did in the testing section above. We are making a GET request just like we would from our browser but now with live data from the sensor
+    if (client.connect(server, 80)) { // check if the connection is successful
+        client.print("GET /write_data.php?"); // GET request
+        client.print("detail="); // GET request
+        client.print(msg); // Message stored in database after getting information from hardware 
         client.println(" HTTP/1.1"); // Part of the GET request
-        client.println("Host: **.**.**.**"); // IMPORTANT: If you are using XAMPP you will have to find out the IP address of your computer and put it here (it is explained in previous article). If you have a web page, enter its address (ie.Host: "www.yourwebpage.com")
+        client.println("Host: **.**.**.**"); // IP address of localhost
         client.println("Connection: close"); // Part of the GET request telling the server that we are over transmitting the message
         client.println(); // Empty line
         client.println(); // Empty line
