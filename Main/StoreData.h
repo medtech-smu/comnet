feff21af273295bb6737ee1e4c445a45701391ed
FediHachicha <fedi.hachicha95@gmail.com>
2017-04-24 15:14:22 +0200

    Update StoreData.h

--- Main/StoreData.h
+++ Main/StoreData.h
@@ -1,5 +1,5 @@
 //localhost ip@
-char server[] = "192.168.43.66"; 
+char server[] = "**.**.**.**"; 
 // Initialize the WiFi server library
 WiFiClient client;
 
@@ -9,7 +9,7 @@ void saveData (String msg){
         client.print("detail="); // This
         client.print(msg); // And this is what we did in the testing section above. We are making a GET request just like we would from our browser but now with live data from the sensor
         client.println(" HTTP/1.1"); // Part of the GET request
-        client.println("Host: 192.168.43.66"); // IMPORTANT: If you are using XAMPP you will have to find out the IP address of your computer and put it here (it is explained in previous article). If you have a web page, enter its address (ie.Host: "www.yourwebpage.com")
+        client.println("Host: **.**.**.**"); // IMPORTANT: If you are using XAMPP you will have to find out the IP address of your computer and put it here (it is explained in previous article). If you have a web page, enter its address (ie.Host: "www.yourwebpage.com")
         client.println("Connection: close"); // Part of the GET request telling the server that we are over transmitting the message
         client.println(); // Empty line
         client.println(); // Empty line
