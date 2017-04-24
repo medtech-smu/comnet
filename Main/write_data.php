08d7c5e0bb17934b16b0715ce8115ef003e449e7
FediHachicha <fedi.hachicha95@gmail.com>
2017-04-24 15:31:42 +0200

    Update write_data.php

--- Main/write_data.php
+++ Main/write_data.php
@@ -1,19 +1,21 @@
+//Place this file in "xampp\htdocs" directory
 <?php
 
     // Prepare variables for database connection
-    $dbname= "doorbell";
-    $dbusername = "arduino";  // enter database username, I used "arduino" in step 2.2
-    $dbpassword = "arduinotest";  // enter database password, I used "arduinotest" in step 2.2
-    $localhost = "192.168.43.66"; // IMPORTANT: if you are using XAMPP enter "localhost", but if you have an online website enter its address, ie."www.yourwebsite.com"
+    $dbname= "******"; // enter database name
+    $dbusername = "************";  // enter database username
+    $dbpassword = "***************";  // enter database password
+    $localhost = "***********"; // IMPORTANT: if you are using XAMPP enter "localhost", but if you have an online website enter its address, ie."www.yourwebsite.com"
 
     // Connect to your database
 
     $dbconnect = mysqli_connect($localhost, $dbusername, $dbpassword, $dbname);
-    $dbselect = mysqli_select_db($dbconnect,"doorbell");
+    $dbselect = mysqli_select_db($dbconnect,$dbname);
 
     // Prepare the SQL statement
-
-    $sql = "INSERT INTO doorbell.data (detail) VALUES ('".$_GET["detail"]."')";    
+    
+    // Insert in *...* the corresponding name
+    $sql = "INSERT INTO *dbname*.*tablename* (*attribute*) VALUES ('".$_GET[" *attribute* "]."')";
 
     // Execute SQL statement
 
